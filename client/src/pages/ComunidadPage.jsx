dcad3dcad8a55c30881eaedd372762ba2a4987cc
AndrésToscano <andress.toscano@gmail.com>
2024-10-09 11:25:30 -0500

    Quienes somos

--- client/src/pages/ComunidadPage.jsx
+++ client/src/pages/ComunidadPage.jsx
@@ -1,8 +1,12 @@
 const ComunidadPage = () => {
     return (
         <div>
-            <section className="flex">
-                <div className=" w-[200px] h-[200px] bg-[#6DAD58]"></div>
+            <section className="flex flex-col">
+                <div className="flex flex-row space-x-8 font-yeseva">
+                    <div className="w-[200px] h-[220px] bg-[#6DAD58] rounded-br-full"></div>
+                    <h2 className="text-[35px]">¿Quienes somos en Agriwave?</h2>
+                </div>
+                <div></div>
             </section>
         </div>
     )
