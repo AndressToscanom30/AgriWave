b4335423ef5fb3599d14be86454ffd82af4f7662
AndrésToscano <andress.toscano@gmail.com>
2024-10-10 23:58:10 -0500

    Error quitado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes} from "react-router-dom"
 import HomePage from './pages/HomePage'
 import NavBar from "./components/NavBar"
-import ContactoPage from "./pages/contactoPage"
 import ComunidadPage from "./pages/ComunidadPage"
 import AyudaPage from "./pages/AyudaPage"
 import Footer from "./components/footer"
@@ -16,7 +15,6 @@ function App() {
     <NavBar />
       <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/contacto" element={<ContactoPage />} />
           <Route path="/comunidad" element={<ComunidadPage />} />
           <Route path="/ayuda" element={<AyudaPage />} />
           <Route path="/inventarioTerreno" element={<Terreno />} />
